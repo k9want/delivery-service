90eaedd1d04b5f34cb954369bc73bd0e7b8075f9
Sangkwon Lee <sangkwon26@gmail.com>
2024-03-25 22:51:20 +0900

    Ch 07-02. Spring Security를 통한 가맹점 서버 개발 - 1

---
+++ store-admin/src/main/java/org/deliveryservice/storeadmin/StoreAdminApplication.java
@@ -0,0 +1,12 @@
+package org.deliveryservice.storeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreAdminApplication.class, args);
+    }
+}
