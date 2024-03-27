603463348d763adc269f687e08caa8ee0d9eaf4b
Sangkwon Lee <sangkwon26@gmail.com>
2024-03-27 11:40:50 +0900

    Ch 08-04. Producer 개발하기 -1

---
+++ api/src/main/java/org/delivery/api/common/rabbitmq/Producer.java
@@ -0,0 +1,17 @@
+package org.delivery.api.common.rabbitmq;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class Producer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void producer(String exchange, String routeKey, Object object) {
+        rabbitTemplate.convertAndSend(exchange, routeKey, object);
+    }
+
+}
